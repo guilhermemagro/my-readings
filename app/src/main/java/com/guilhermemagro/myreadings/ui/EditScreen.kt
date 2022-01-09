99e20269e71e12500fec4da598271d9e8962f6bc
Guilherme Magro <guilherme.magro@hotmail.com>
2022-01-09 11:14:37 -0300

    Improved EditScreen layout

--- app/src/main/java/com/guilhermemagro/myreadings/ui/EditScreen.kt
+++ app/src/main/java/com/guilhermemagro/myreadings/ui/EditScreen.kt
@@ -4,25 +4,30 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.guilhermemagro.myreadings.R
 import com.guilhermemagro.myreadings.data.entities.Book
@@ -45,9 +50,10 @@ fun EditScreen(
                 totalPagesTextState != "" &&
                 currentPageTextState != ""
 
+        val padding = 16.dp
         Column(
-            modifier = Modifier.padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            modifier = Modifier.padding(padding),
+            verticalArrangement = Arrangement.spacedBy(padding)
         ) {
             TextField(
                 modifier = Modifier.fillMaxWidth(),
@@ -76,7 +82,12 @@ fun EditScreen(
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Button(
-                    onClick = { onDeleteBook(book) }
+                    onClick = { onDeleteBook(book) },
+                    modifier = Modifier.weight(1f, true),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFFEC532F),
+                        contentColor = Color.White
+                    )
                 ) {
                     Icon(
                         Icons.Filled.Delete,
@@ -86,6 +97,7 @@ fun EditScreen(
                     Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
                     Text(stringResource(R.string.edit_screen_delete))
                 }
+                Spacer(modifier = Modifier.size(padding))
                 Button(
                     onClick = {
                         onUpdateBook(
@@ -97,8 +109,19 @@ fun EditScreen(
                             )
                         )
                     },
+                    modifier = Modifier.weight(1f, true),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFF78D865),
+                        contentColor = Color.White
+                    ),
                     enabled = allFieldsFilled
                 ) {
+                    Icon(
+                        Icons.Filled.Edit,
+                        contentDescription = stringResource(R.string.edit_screen_edit_icon),
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
                     Text(stringResource(R.string.edit_screen_edit))
                 }
             }
@@ -106,4 +129,16 @@ fun EditScreen(
     } ?: run {
         Text(text = "Something went wrong! Try again later! =/")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EditScreenPreview() {
+    EditScreen(bookAndRecords = BookAndRecords(
+        book = Book(
+            title = "Título livro",
+            totalPages = 300,
+            currentPage = 100
+        ), records = listOf()
+    ), onDeleteBook = {}, onUpdateBook = {})
 }
