00a2401ab666a39ff4f065b5862d80c4f283bb6e
Guilherme Magro <guilherme.magro@hotmail.com>
2022-01-15 16:21:33 -0300

    Implemented FAB on HomeScreen

--- app/src/main/java/com/guilhermemagro/myreadings/ui/HomeScreen.kt
+++ app/src/main/java/com/guilhermemagro/myreadings/ui/HomeScreen.kt
@@ -3,19 +3,24 @@ package com.guilhermemagro.myreadings.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import com.guilhermemagro.myreadings.R
 import com.guilhermemagro.myreadings.data.entities.Book
 import com.guilhermemagro.myreadings.data.entities.BookAndRecords
+import com.guilhermemagro.myreadings.utils.DEFAULT_FAB_SIZE
 
 @Composable
 fun HomeScreen(
@@ -39,15 +45,19 @@ fun HomeScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "MyReadings")
+                    Text(text = stringResource(R.string.home_screen_title))
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onRegistrationClick) {
+                Icon(Icons.Filled.Add, stringResource(id = R.string.home_screen_fab))
+            }
         }
     ) {
         HomeScreenContent(
             scaffoldState = scaffoldState,
             booksAndRecords = booksAndRecords,
-            onRegistrationClick = onRegistrationClick,
             onBookCardClick = onBookCardClick
         )
     }
@@ -57,7 +67,6 @@ fun HomeScreen(
 fun HomeScreenContent(
     scaffoldState: ScaffoldState,
     booksAndRecords: List<BookAndRecords>? = null,
-    onRegistrationClick: () -> Unit,
     onBookCardClick: (Book) -> Unit
 ) {
     val scrollState = rememberLazyListState()
@@ -77,20 +86,18 @@ fun HomeScreenContent(
                 items(booksAndRecords) { bookAndRecords ->
                     BookCard(book = bookAndRecords.book, onBookCardClick = onBookCardClick)
                 }
+                item {
+                    Spacer(modifier = Modifier.height(DEFAULT_FAB_SIZE.dp))
+                }
             } ?: run {
                 item {
                     Column(
                         modifier = Modifier.fillMaxWidth(),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        Icon(Icons.Filled.AddCircle, null)
+                        Icon(Icons.Filled.Warning, null)
                         Text(stringResource(R.string.home_screen_empty_books))
                     }
-                }
-            }
-            item {
-                Button(onClick = { onRegistrationClick() }) {
-                    Text(text = "Cadastrar novo livro")
                 }
             }
         }
@@ -102,7 +109,6 @@ fun HomeScreenContent(
 fun BodyContentPreview() {
     HomeScreenContent(
         scaffoldState = rememberScaffoldState(),
-        onRegistrationClick = {},
         onBookCardClick = {}
     )
 }
--- app/src/main/java/com/guilhermemagro/myreadings/utils/Constants.kt
+++ app/src/main/java/com/guilhermemagro/myreadings/utils/Constants.kt
@@ -1,3 +1,4 @@
 package com.guilhermemagro.myreadings.utils
 
 const val DATABASE_NAME = "myreadings-db"
+const val DEFAULT_FAB_SIZE = 56
